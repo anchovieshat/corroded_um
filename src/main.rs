382ff3fd6569a6bfeb7e123721d07eccf6961b8f
Colin Davidson <colin@pentaquine.com>
2017-04-29 08:36:58 -0400

    Handle input, speed up ops by reducing work

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ extern crate byteorder;
 use std::fs::File;
 use std::io::Read;
 use std::io::Cursor;
+use std::io::stdin;
 
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+
+macro_rules! reg {
+    (a, $e:expr) => ((($e >> 6) & 7) as usize);
+    (b, $e:expr) => ((($e >> 3) & 7) as usize);
+    (c, $e:expr) => (($e & 7) as usize);
+}
 
 fn main() {
     let mut f = File::open("sandmark.umz").unwrap();
@@ -22,69 +29,92 @@ fn main() {
         }
     }
 
+    let mut pc = 0;
     let mut reg = [0u32; 8];
     let mut memory: Vec<Vec<u32>> = vec![];
     memory.push(program);
-    let mut pc = 0;
+    let mut outfile: Option<File> = None;
 
     let mut running = true;
-
     while running {
         let inst = memory[0][pc as usize];
-        pc += 1;
 
         let op = inst >> 28;
-        let a = (inst >> 6) & 7;
-        let b = (inst >> 3) & 7;
-        let c = (inst >> 0) & 7;
 
+        //println!("[{}] {} {:?}: {}, {}, {}", pc, op, reg, a, b, c);
+
+        pc += 1;
         match op {
             0 => {
-                if reg[c as usize] != 0 {
-                    reg[a as usize] = reg[b as usize];
+                if reg[reg!(c, inst)] != 0 {
+                    reg[reg!(a, inst)] = reg[reg!(b, inst)];
                 }
             },
             1 => {
-                reg[a as usize] = memory[reg[b as usize] as usize][reg[c as usize] as usize];
+                reg[reg!(a, inst)] = memory[reg[reg!(b, inst)] as usize][reg[reg!(c, inst)] as usize];
             },
             2 => {
-                memory[reg[a as usize] as usize][reg[b as usize] as usize] = reg[c as usize];
+                /*if (reg[a as usize] == 0) {
+                    println!("Self-modifiying instruction {}", reg[b as usize]);
+                }*/
+
+                memory[reg[reg!(a, inst)] as usize][reg[reg!(b, inst)] as usize] = reg[reg!(c, inst)];
             },
             3 => {
-                reg[a as usize] = reg[b as usize].wrapping_add(reg[c as usize]);
+                reg[reg!(a, inst)] = reg[reg!(b, inst)].wrapping_add(reg[reg!(c, inst)]);
             },
             4 => {
-                reg[a as usize] = reg[b as usize].wrapping_mul(reg[c as usize]);
+                reg[reg!(a, inst)] = reg[reg!(b, inst)].wrapping_mul(reg[reg!(c, inst)]);
             },
             5 => {
-                reg[a as usize] = reg[b as usize] / reg[c as usize];
+                reg[reg!(a, inst)] = reg[reg!(b, inst)].wrapping_div(reg[reg!(c, inst)]);
             },
             6 => {
-                reg[a as usize] = !(reg[b as usize] & reg[c as usize]);
+                reg[reg!(a, inst)] = !(reg[reg!(b, inst)] & reg[reg!(c, inst)]);
             },
             7 => {
                 running = false;
             },
             8 => {
-                let vec_size = reg[c as usize] as usize;
-                reg[b as usize] = memory.len() as u32;
+                let vec_size = reg[reg!(c, inst)] as usize;
+                reg[reg!(b, inst)] = memory.len() as u32;
                 memory.push(vec![0u32; vec_size]);
             },
             9 => {
-                memory[reg[c as usize] as usize] = vec![];
+                memory[reg[reg!(c, inst)] as usize] = vec![];
             },
-            10 => { print!("{}", reg[c as usize] as u8 as char); },
-            11 => { unimplemented!(); },
-            12 => {
-                if reg[b as usize] != 0 {
-                    memory[0] = memory[reg[b as usize] as usize].clone();
+            10 => {
+                let ch = reg[reg!(c, inst)] as u8;
+                match outfile {
+                    Some(ref mut file) => {
+                        file.write_u8(ch).unwrap();
+                    }
+                    None => {
+                        print!("{}", ch as char);
+                    }
                 }
-                pc = reg[c as usize];
+            },
+            11 => {
+                match stdin().bytes().next() {
+                    Some(x) => {
+                        let ch = x.unwrap();
+
+                        reg[reg!(c, inst)] = (ch as u32) & 0xFF;
+                    }
+                    None => { reg[reg!(c, inst)] = 0xFFFFFFFF; }
+                }
+            },
+            12 => {
+                if reg[reg!(b, inst)] != 0 {
+                    memory[0] = memory[reg[reg!(b, inst)] as usize].clone();
+                }
+
+                pc = reg[reg!(c, inst)];
             },
             13 => {
-                let x = (inst >> 25) & 7;
+                let x = ((inst >> 25) & 7) as usize;
                 let num = inst & 0x01FFFFFF;
-                reg[x as usize] = num;
+                reg[x] = num;
             },
             _ => { unimplemented!(); }
         }
